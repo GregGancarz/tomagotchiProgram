a98fa9e124199352128fe8e097b71d0045f3046b
Greg Gancarz <propterprivatum@tuta.io>
2019-04-01 16:13:36 -0500

    Name is displayed

--- js/app.js
+++ js/app.js
@@ -14,16 +14,32 @@ class Toma {
 	}
 	changeName(){
 		const $usersName = $(`#new-name`).val();
-		this.name = $usersName
+		this.name = $usersName;
+		$(`#name`).text(this.name);
 	}
 }
 
 const toma1 = new Toma();
 
+$(`#hunger`).text(toma1.hunger);
+$(`#sleepiness`).text(toma1.sleepiness);
+$(`#boredom`).text(toma1.boredom);
+$(`#age`).text(toma1.name);
+
+
+
+
+
+
 const game = {
 	lights: true,
 	time: 0
 }
+
+
+
+
+
 
 $('#name-button').on('click', () => {
 	toma1.changeName();
