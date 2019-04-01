81113d700e6d8e4d02c00a59f1370bea346395a8
Greg Gancarz <propterprivatum@tuta.io>
2019-04-01 16:01:40 -0500

    can now name it

--- js/app.js
+++ js/app.js
@@ -3,13 +3,28 @@ class Toma {
 		this.hunger = 0;
 		this.sleepiness = 0;
 		this.boredom = 0;
-		this.age = 0
+		this.age = 0;
+		this.name = '';
+	}
+	feed(){
+		console.log("feed invoked");
+	}
+	playWith(){
+		console.log("Play with invoked");
+	}
+	changeName(){
+		const $usersName = $(`#new-name`).val();
+		this.name = $usersName
 	}
 }
 
-const Toma1 = new Toma();
+const toma1 = new Toma();
 
 const game = {
 	lights: true,
-	
+	time: 0
 }
+
+$('#name-button').on('click', () => {
+	toma1.changeName();
+});
